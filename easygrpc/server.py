92909a17b1e5ed71ac8d449d334e1d44c92f998c
4kpt <muatai@rambler.ru>
2016-10-05 08:44:31 +0300

    Change: Use only route attribute in server creator.

--- easygrpc/server.py
+++ easygrpc/server.py
@@ -368,3 +368,8 @@ class GRPCServer(object):
                 time.sleep(sleep_time or self.SERVER_TIMEOUT_SLEEP)  # one day in seconds
         except KeyboardInterrupt:
             self._server.stop(0)
+
+
+from .. import grpcadmin
+print(grpcadmin)
+
